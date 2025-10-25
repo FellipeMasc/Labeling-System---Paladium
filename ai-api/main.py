b7c2b98645fe45d55ba7094416a56b6c1b5d01a9
FellipeMasc <fellipe_spd@hotmail.com>
2025-10-25 09:18:25 -0300

    chore(ai-api): change to class and change type return

--- ai-api/main.py
+++ ai-api/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-from app.deps.inference_agent import infer_image_tags
-app = FastAPI()
+from app.deps.inference_agent import inference_agent
+from contextlib import asynccontextmanager
+from app.schemas.agent_schema import TagInferenceRequest
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global agent
+    agent = inference_agent()
+    yield
+    
+
+app = FastAPI(lifespan=lifespan)
 
 @app.post("/infer-tags")
 async def infer_tags(image_url: str):
-    tags = await infer_image_tags(image_url)
-    return {"tags": tags}
+    tags = agent.infer_image_tags(TagInferenceRequest(image_url=image_url))
+    return tags
