8e2a2e0452ad62fcef47b643b4a4896d510f7bda
FellipeMasc <fellipe_spd@hotmail.com>
2025-10-25 09:19:23 -0300

    wip

--- ai-api/app/deps/inference_agent.py
+++ ai-api/app/deps/inference_agent.py
@@ -33,29 +33,3 @@ class inference_agent:
         return response_parsed.tags
     
     
-# async def infer_image_tags(TagInferenceRequest: TagInferenceRequest) -> List[str]:
-#     """
-#     Call OpenAI Vision API to infer up to 3 tags describing the image content.
-#     """
-#     prompt = (
-#         "You are a helpful assistant. "
-#         "Given this image, provide a comma-separated list of 3 possible tags describing what the image contains. "
-#         "Be brief and generic. Example: 'cat, window, sunlight'"
-#     )
-
-#     response = client.responses.parse(
-#         model="gpt-4.1-mini",
-#         input=[{
-#             "role": "user",
-#             "content": [
-#                 {"type": "input_text", "text": prompt},
-#                 {
-#                     "type": "input_image",
-#                     "image_url": image_url,
-#                 },
-#             ],
-#         }],
-#         text_format=TagInferenceResponse,
-#     )
-#     array_tags = json.loads(response.output_text).get("tags",[])
-#     return array_tags
