8f093b112afa3f5cb10d9ab2918d21c7473cbdf5
Fellipe Mascarenhas <102706880+FellipeMasc@users.noreply.github.com>
2025-10-28 09:31:11 -0300

    chore(ai-api): change to openai (#12)

--- ai-api/app/deps/hugging_face_embbeding.py
+++ ai-api/app/deps/hugging_face_embbeding.py
@@ -1,9 +1,12 @@
 from typing import List
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
+from app.deps.inference_agent import inference_agent
 
 def calculate_embedding(label : str) -> List[float]:
     """
     Given a label, return the embedding for the label.
     """
     text = f"current tag: {label}"
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2").embed_query(text)
+    agent = inference_agent().get_client()
+    return agent.embeddings.create(input=text, model="text-embedding-3-small", dimensions=1536).data[0].embedding
+    # return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2").embed_query(text)
